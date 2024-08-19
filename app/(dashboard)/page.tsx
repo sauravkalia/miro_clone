e8254039efb91fa4dac1bd0efb79811adf5f7040
Saurav Kalia <sauravhunny@gmail.com>
2024-08-19 16:51:40 +0530

    adding board list

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,9 +1,32 @@
-const DashboardPage = () => {
+"use client";
+
+import { useOrganization } from "@clerk/nextjs";
+import { EmptyOrg } from "./_components/empty-org";
+
+interface DashboardPageProps {
+    searchParams: {
+        search?: string;
+        favorites?: string;
+    };
+};
+
+const DashboardPage = ({
+    searchParams,
+}: DashboardPageProps) => {
+    const { organization } = useOrganization();
+
     return (
-        <div>
-            <h1>Dashboard</h1>
+        <div className="flex-1 h-[calc(100%-80px)] p-6">
+            {JSON.stringify(searchParams)}
+            {!organization ? (
+                <EmptyOrg />
+            )
+                : (
+                    <p>Board List!</p>
+                )
+            }
         </div>
-    )
-}
+    );
+};
 
 export default DashboardPage;
